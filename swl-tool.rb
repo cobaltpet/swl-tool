b2d0d8a7237f3afee208638f345809d8c34e1a3f
Eric Weatherall <cobaltpet@gmail.com>
2017-09-03 17:55:06 -0700

    Updated version timestamp.

--- swl-tool.rb
+++ swl-tool.rb
@@ -38,7 +38,7 @@ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND 
 Author = "Eric Weatherall"
 AuthorEmail = "cobaltpet gmail com"
 AuthorBlog = "http://cobaltpet.blogspot.com/"
-ScriptVersion = "2017-09-04 0045UTC"
+ScriptVersion = "2017-09-04 0054UTC"
 
 ### Required executables
 
